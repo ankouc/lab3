59a4a9a5ae55f236a14cc34d8229b2e2b0fe7c48
ankouc <aniss.k45@gmail.com>
2016-04-04 18:06:21 -0700

    pretty satisfied

--- main.cpp
+++ main.cpp
@@ -7,16 +7,18 @@
 
 #include <random>
 
+#define MIN(x,y) ((x)<(y)?(x):(y))
 
 using namespace std;
 
+int global_timeout = 180*3;
 // returns the number of students that can not see
-unsigned int cost_function(int* solution, int size){
+unsigned int cost_function(unsigned int* students,unsigned int* solution,unsigned  int size){
   unsigned int cost = 0;
-  unsigned int current = solution[0];
-  for(int i = 1 ; i < size ; i++){
-    if(solution[i] > current)
-      current = solution[i];
+  unsigned int current = students[solution[0]];
+  for(unsigned int i= 1 ; i < size ; i++){
+    if(students[solution[i]] > current)
+      current = students[solution[i]];
     else
       cost++;
   }
@@ -25,8 +27,6 @@ unsigned int cost_function(int* solution, int size){
 
 int main(int argc, char** argv){
   srand(time(0));
-  default_random_engine generator;
-  generator.seed(time(0));
   int verbose = 0;
   if(argc > 3)
     verbose = 1;
@@ -35,16 +35,14 @@ int main(int argc, char** argv){
   f.open(argv[2]);
   unsigned int n_of_students;
   f >> n_of_students; 
-//  printf("[n_of_students=%d]\n",n_of_students);
 
   unsigned int n_of_safe_pairs;
   f >> n_of_safe_pairs;
-// printf("[n_of_safe_pairs=%d]\n",n_of_safe_pairs);
 
   unsigned int* students = new unsigned int[n_of_students];
   unsigned int** adjacency_matrix = new unsigned int*[n_of_students];
   unsigned int max_heigth = 0;
-  for(unsigned int i = 0; i < n_of_students; i++){
+  for(unsigned int i= 0; i < n_of_students; i++){
     f >> students[i];
     if(max_heigth < students[i])
       max_heigth = students[i];
@@ -53,7 +51,7 @@ int main(int argc, char** argv){
       adjacency_matrix[i][j] = 0;
   }
 
-  for(unsigned int i = 0; i < n_of_safe_pairs; i++){
+  for(unsigned int i= 0; i < n_of_safe_pairs; i++){
     int a, b;
     f >> a >> b;
     adjacency_matrix[a-1][b-1] = 1;
@@ -61,10 +59,10 @@ int main(int argc, char** argv){
   }
 
   if(verbose){
-    for(unsigned int i = 0; i < n_of_students; i++)
+    for(unsigned int i= 0; i < n_of_students; i++)
       cout << setw(2) << i ;
     cout << endl;
-    for(unsigned int i = 0; i < n_of_students; i++){
+    for(unsigned int i= 0; i < n_of_students; i++){
       for(unsigned int j = 0; j < n_of_students; j++)
         cout << setw(2) << adjacency_matrix[i][j] ;
       cout << "--" << i << endl;
@@ -73,18 +71,19 @@ int main(int argc, char** argv){
 
   unsigned int *solution = new unsigned int[n_of_students];
   unsigned int *placed = new unsigned int[n_of_students];
-  int hello = 1;
+  int continue_looping = 1;
 
-  while(hello){
-    for(unsigned int i = 0; i < n_of_students; i++)
+  while(continue_looping){
+    for(unsigned int i= 0; i < n_of_students; i++)
       placed[i] = 0;
     unsigned int restart = 0;
-    unsigned int index = rand() % n_of_students; 
+    unsigned int index = (int)((double)rand()/RAND_MAX * n_of_students); 
     solution[0] = index;
-    for(unsigned int i = 1; i < n_of_students; i++){
+    placed[index] = 1;
+    for(unsigned int i= 1; i < n_of_students; i++){
       while(1){
         unsigned int timeout = 0;
-        unsigned int current = rand() % n_of_students;
+        unsigned int current = (unsigned int)((double)rand()/RAND_MAX * n_of_students);
         while(adjacency_matrix[index][current] == 0
           || placed[current] == 1){
           current = (current+1) % n_of_students;
@@ -107,27 +106,39 @@ int main(int argc, char** argv){
         break;
       }
       if(i == n_of_students-1)
-        hello = 0;
+        continue_looping = 0;
     }
   }
+  {
+    unsigned int *reverse = (unsigned int*)malloc(n_of_students*sizeof(unsigned int));
+    for(unsigned int i= 0 ; i < n_of_students ; i++)
+      reverse[i] = solution[n_of_students-1-i];
+    unsigned int reverse_cost = cost_function(students,reverse,n_of_students);
+    unsigned int cost = cost_function(students,solution,n_of_students);
+    if(reverse_cost < cost){
+      memcpy(solution,reverse,n_of_students*sizeof(unsigned int));
+    }
+    free(reverse);
+  }
   time_t current_time = time(0);
-  while(time(0)-current_time < 60){
+  unsigned int probability = 0;
+  unsigned int cost = cost_function(students,solution,n_of_students);
+  while(time(0)-current_time < global_timeout){
     unsigned int* potentiel = new unsigned int[n_of_students];
     memcpy(potentiel,solution,n_of_students*sizeof(unsigned int));
     unsigned int retries = n_of_students;
     while(retries--){
-      unsigned int a = rand() % n_of_students;
-      while(a == 0 || a == n_of_students-1)
-        a = rand() % n_of_students;
-      unsigned int b = rand() % n_of_students;
+      unsigned int a = (unsigned int)((double) rand()/RAND_MAX * n_of_students);
+      while(a == n_of_students-1)
+        a = (unsigned int)((double)rand()/RAND_MAX * n_of_students);
+      unsigned int b = (unsigned int)((double)rand()/RAND_MAX * n_of_students);
       int restart = 0;
-      for(int i = 0 ; i < n_of_students ; i++)
-        if(students[potentiel[b]] > students[potentiel[a]]
+      for(unsigned int i= 0 ; i < n_of_students ; i++)
+        if(students[potentiel[b]] < students[potentiel[a]]
               || potentiel[b] < potentiel[a]
-              || b == n_of_students-1
               || b == 0
               || potentiel[b] == potentiel[a]){
-          b = rand() % n_of_students;
+          b = (unsigned int)((double)rand()/RAND_MAX * n_of_students);
           if(i == n_of_students-1)
             restart = 1;
         }
@@ -137,22 +148,73 @@ int main(int argc, char** argv){
       if(restart)
         continue;
 
-      if( adjacency_matrix[potentiel[a]][potentiel[b-1]] == 1
-        && adjacency_matrix[potentiel[a]][potentiel[b+1]] == 1
-        && adjacency_matrix[potentiel[b]][potentiel[a-1]] == 1
-        && adjacency_matrix[potentiel[b]][potentiel[a+1]] == 1){
-        unsigned int temp = potentiel[a];
-        potentiel[a] = potentiel[b];
-        potentiel[b] = temp;
-        break;
+      if(a == 0 && b == n_of_students-1){
+        if( adjacency_matrix[potentiel[a]][potentiel[b-1]] == 1
+          && adjacency_matrix[potentiel[b]][potentiel[a+1]] == 1){
+          unsigned int temp = potentiel[a];
+          potentiel[a] = potentiel[b];
+          potentiel[b] = temp;
+//          break;
+          }
+      }
+      else if(a == 0){
+        if(adjacency_matrix[potentiel[a]][potentiel[b-1]] == 1
+          && adjacency_matrix[potentiel[a]][potentiel[b+1]] == 1
+          && adjacency_matrix[potentiel[b]][potentiel[a+1]] == 1){
+          unsigned int temp = potentiel[a];
+          potentiel[a] = potentiel[b];
+          potentiel[b] = temp;
+//          break;
+        }
+      }
+      else if(b == n_of_students-1){
+        if(adjacency_matrix[potentiel[b]][potentiel[a-1]] == 1
+          && adjacency_matrix[potentiel[b]][potentiel[a+1]] == 1
+          && adjacency_matrix[potentiel[a]][potentiel[b-1]] == 1){
+          unsigned int temp = potentiel[a];
+          potentiel[a] = potentiel[b];
+          potentiel[b] = temp;
+//          break;
+        }
+      }
+      else if( a != 0 && b != n_of_students-1){
+        if( adjacency_matrix[potentiel[a]][potentiel[b-1]] == 1
+          && adjacency_matrix[potentiel[a]][potentiel[b+1]] == 1
+          && adjacency_matrix[potentiel[b]][potentiel[a-1]] == 1
+          && adjacency_matrix[potentiel[b]][potentiel[a+1]] == 1){
+          unsigned int temp = potentiel[a];
+          potentiel[a] = potentiel[b];
+          potentiel[b] = temp;
+//          break;
+        }
       }
     }
-    unsigned int cost = cost_function(solution,n_of_students);
-    unsigned int potentiel_cost = cost_function(potentiel,n_of_students);
-    if(cost > potentiel_cost){
-      memcpy(solution,potentiel,n_of_students*sizeof(unsigned int));
-      cout << cost << ":" << potentiel_cost << endl;
+    unsigned int *reverse = (unsigned int*)malloc(n_of_students*sizeof(unsigned int));
+    for(unsigned int i= 0 ;i < n_of_students ; i++)
+      reverse[i] = potentiel[n_of_students-1-i];
+    unsigned int potentiel_cost = cost_function(students,potentiel,n_of_students);
+    unsigned int reverse_cost = cost_function(students,reverse,n_of_students);
+    if(reverse_cost < potentiel_cost){
+      memcpy(potentiel,reverse,n_of_students*sizeof(unsigned int));
+      potentiel_cost = reverse_cost;
     }
+    free(reverse);
+
+    cost = cost_function(students,solution,n_of_students);
+    //ankouc
+    if((((double)rand()/RAND_MAX)*global_timeout/(double)(potentiel_cost - cost) > probability*2 && potentiel_cost >= cost)
+      || cost > potentiel_cost)
+      memcpy(solution,potentiel,n_of_students*sizeof(unsigned int));
+
+//    cout << cost << ":" << potentiel_cost << endl;
+    probability = time(0) - current_time ;
+//    for(unsigned int i= 1 ;i < n_of_students ; i++)
+//      if( adjacency_matrix[solution[i]][solution[i-1]] != 1)
+//        cout << "==============" << endl;
   }
+
+  cout << "nombre d etudiant qui peuvent voir " << n_of_students-cost << endl;
+//  for(unsigned int i= 0 ; i < n_of_students; i++)
+//    cout << solution[i]+1 << "\t" << students[solution[i]] << endl;
   return 0;
 }
